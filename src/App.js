c19a38f9a3ae2d95784e220f49aec4804f4e0895
pankaj35 <pankajehra1001@gmail.com>
2020-03-18 17:31:19 +0530

    reactRouter

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React , {Component} from 'react';
+import { BrowserRouter as Router, Switch,Route,Link } from "react-router-dom";
+import './App.css';
+
+import Home from './home';
+import Message from './message';
+import Contact from './contact'
+
+class App extends Component {
+  render(){
+    return(
+      <Router>
+        <div>
+          <ul>
+            <li>
+               <Link to="/">routing link</Link>
+            </li>
+            <li>
+            <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/message">Message</Link>
+            </li>
+          </ul>
+
+          <switch>
+            <route exact path = "/">routing</route>
+            <Route exact path ="/home"><Home /></Route>
+            <Route exact path ="/contact"><Contact /></Route>
+            <Route exact path ="/message"><Message /></Route>
+          </switch>
+          </div>
+      </Router>
+    )
+  }
+}
+export default App;
