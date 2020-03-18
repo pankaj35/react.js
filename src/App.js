49bb170097a0bd31fa9937138a405bdc4078e5e6
pankaj35 <pankajehra1001@gmail.com>
2020-03-18 13:48:58 +0530

    todo app

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React , {Component} from 'react';
+import './App.css';
+import InputChecked from './InputChecked';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+     
+      Names: [
+        {
+          id:0,
+          name: "Football",
+          checked:false
+        },
+        {
+          id:1,
+          name: "cricket",
+          checked:false
+        },
+        {
+          id:2,
+          name: "Hockey",
+          checked:false
+        },
+        {
+          id:3,
+          name: "Basketball",
+          checked:false
+        },
+      ],
+      text: "",
+      
+    }
+    this.handlehange = this.handlehange.bind(this);
+  }
+  
+  handlehange = (ID) => {
+    let data = this.state.Names;
+    console.log(data);
+    // console.log(event.target.name);
+    data = data.map(val=>{
+      console.log(ID)
+      if(val.id === ID){
+        val.checked = !val.checked;
+      }
+      return val;
+     
+    })
+    console.log(data);
+    
+    this.setState({Names: data})
+}
+submit = (event)=>{
+  event.preventDefault();
+  const text = this.state.text
+  // this.setState(
+  //   {Names: {...this.state.Names, }}
+  // )
+  // this.setState((prev)=>({Names:[...prev.Names, {id: this.state.Names.length, name:text }]}))
+  if(text === ""){
+    // const newConst = [...this.state.text,newConst]
+    this.setState({
+      text: "",
+    })
+  }else{
+    this.setState((prev)=>({Names:[...prev.Names, {id: this.state.Names.length, name:text }]}))
+  }
+  
+   
+}
+textcontext = (e) =>{
+  this.setState (
+    {text: e.target.value}
+  )
+}
+editable = (e,index) =>{
+  console.log(index);
+  let Names = this.state.Names;
+  Names[index].name = e.target.value;
+   
+  this.setState({
+    Names
+  })
+}
+  render() {
+    console.log(this.state.Names);
+    return(
+      <div>
+        <h1>Todo App</h1>
+       <InputChecked name= {this.state.Names} handlehange={this.handlehange} editable ={this.editable}/>
+      <form onSubmit ={this.submit}>
+       <input  type="text" value = {this.state.text} onChange={this.textcontext}/>
+      <button>submit</button>
+      </form>
+      </div>
+ 
+    )
+  }
+}
+
+export default App;
---
+++ src/InputChecked.js
@@ -0,0 +1,47 @@
+import React , {Component} from 'react';
+// import ReactDOM from 'react-dom';
+import ContentEditable from 'react-contenteditable'
+import { Table, Button } from 'semantic-ui-react'
+
+class InputChecked extends Component {
+    
+    // onsubmit = (e) => {
+    //     e.preventDefault();
+    //     // const varai = document.getElementById("inputs");
+    //     // if (varai.length < 0){
+    //     //     varai.style.border = 'border 2px solid';
+    //     // }
+
+    //     const name = ReactDOM.findDOMNode(this._nameInput).value;
+    //     console.log(name);
+
+
+
+         
+    // }
+    
+    render() {
+        return(
+            <div>
+                 {this.props.name.map((item, index) => {
+                     return (
+                     <div  key={item.id}> 
+                        <input type="checkbox" name={item.id} checked={item.checked} onChange={()=> this.props.handlehange(index)} className = "inputs"/>
+                        
+                        <input value={item.name} type="text" onChange ={(e)=> this.props.editable(e,index)}></input>
+                        <span>{item.checked ? "Completed" : null} </span>
+                    </div>
+                     )
+                 })
+
+            }
+         
+               
+             
+           </div>
+        )
+    }
+}
+
+export default InputChecked;
+
